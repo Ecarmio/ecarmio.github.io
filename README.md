27e8d42794b75a1895f00b4670d9314f37752562
Ecarmio <akshaysanal2035@gmail.com>
2025-03-17 20:33:02 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# ecarmio.github.io
-This is the 6th iteration of a Website I've been trying to make past 2 weeks
--- ecarmio/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://yourusername.github.io/", // Replace with your GitHub Pages URL
-  base: "/your-repo-name/", // Replace with your repo name
-  output: "static",
-});
